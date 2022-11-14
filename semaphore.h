68c6c96efe76d9910407769a976331dfca7a6f01
sammycosta <arminetah@hotmail.com>
2022-11-14 19:59:36 -0300

    comentarios

--- semaphore.h
+++ semaphore.h
@@ -14,23 +14,42 @@ public:
     Semaphore(int v = 1);
     ~Semaphore();
 
+    /* Este método deve implementar a operação p (ou sleep) de um semáforo. Deve-se decrementar o
+     * inteiro do semáforo de forma atômica (utilizando fdec) e colocar a Thread para
+     * dormir caso a mesma não conseguir acessar o semáforo (já existir em uso por outra Thread). */
     void p();
+
+    /* Este método deve implementar a operação v (ou wakeup) de um semáforo. Deve-se
+     * incrementar o inteiro do semáforo de forma atômica (utilizando finc) e acordar
+     * uma Thread que estiver dormindo no semáforo.*/
     void v();
+
 private:
     // Atomic operations
-    int finc(volatile int & number);
-    int fdec(volatile int & number);
+
+    /* Incremento atômico utilizando xadd e lock do assembly */
+    int finc(volatile int &number);
+
+    /* Decremento atômico utilizando xadd e lock do assembly */
+    int fdec(volatile int &number);
 
     // Thread operations
+
+    /* O método sleep() deve colocar a Thread que não conseguir acessar o semáforo para dormir e
+     * mudar seu estado para WAITING (note que WAITING é diferente de SUSPENDED do trabalho
+     * trabalho anterior).*/
     void sleep();
+
+    /* O método wakeup() deve acordar uma Thread que estava dormindo no semáforo. */
     void wakeup();
+
+    /* O método wakeup_all() deve acordar todas as Thread que estavam dormindo no semáforo. */
     void wakeup_all();
 
 private:
-    //DECLARAÇÃO DOS ATRIBUTOS DO SEMÁFORO
+    // DECLARAÇÃO DOS ATRIBUTOS DO SEMÁFORO
 };
 
 __END_API
 
 #endif
-
